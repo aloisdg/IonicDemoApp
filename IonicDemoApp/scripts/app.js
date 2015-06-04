2a765c7ab304206b0601b85f50db784f797708db
aloisdg <aloisdegouvello@live.fr>
2015-06-04 18:22:25 +0200

    Update demo

---
+++ IonicDemoApp/scripts/app.js
@@ -0,0 +1,8 @@
+//var App = angular.module("App", ["ionic"]);
+//App.controller("AppCtrl", ["$scope", "$log", AppCtrl]);
+//function  AppCtrl($scope, $log) {
+//    $scope.refresh = () =>
+//	alert("Button pressed");
+//}
+// https://www.youtube.com/watch?v=sCnGSOaaZFo 
+//# sourceMappingURL=app.js.map
